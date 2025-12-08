ab7d2318135e70bdc14675143f3a7dcea87b8664
Jishaan Khatri <jishaan123465@gmail.com>
2025-12-08 23:51:23 +0530

    Iterator Introduction

---
+++ src/lec17/Iterator_Intro.java
@@ -0,0 +1,18 @@
+package lec17;
+import java .util.*;
+public class Iterator_Intro {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> it=list.iterator();
+        while (it.hasNext()){
+            if(it.next()==3) {
+                it.remove();
+            }
+        }
+        System.out.print(list);
+    }
+}
---
+++ src/lec17/List_Iterator.java
@@ -0,0 +1,28 @@
+package lec17;
+import java.util.*;
+public class List_Iterator {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        ListIterator<Integer> it=list.listIterator();
+        System.out.print("Forward : ");
+        while (it.hasNext()){
+            int x= it.next();
+            System.out.print(x+" ");
+        }
+        System.out.println();
+
+        System.out.print("Backward : ");
+        while (it.hasPrevious()){
+            int x=it.previous();
+            System.out.print(x+" ");
+        }
+
+    }
+}
