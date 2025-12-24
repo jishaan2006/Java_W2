7bb05ba3194993566f6ebcb1f809c60d2591aef5
Jishaan Khatri <jishaan123465@gmail.com>
2025-12-24 21:11:06 +0530

    HashSet

--- src/lec16/deleteNumber.java
+++ src/lec16/deleteNumber.java
@@ -10,6 +10,7 @@ public class deleteNumber {
         set.add(5);
         set.add(6);
         set.add(7);
+        set.add(8);
         System.out.println("Set :"+set);
         set.remove(2);
         System.out.println("Set after Deleting 2:"+set);
